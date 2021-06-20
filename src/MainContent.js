2e088d67ffbb5e4fb50d7c3eb7620f1b148cd25c
esteban <esteban.marin@adidas.com>
2021-06-20 10:44:28 -0500

    chore: showing in ui

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,48 +1,18 @@
 import React, { useReducer, useEffect } from "react";
-import { InnerContainer, Input } from "./styles";
+import { InnerContainer, Input } from "./InputComponent";
 import axios from "axios"
-
-function useFetchUsers(username = "ray_benigno") {
-  const [users, setUsers] = React.useState();
-  const [error, setError] = React.useState();
-  const [isLoading, setIsLoading] = React.useState();
-
-  React.useEffect(() => {
-    async function fetchUser() {
-      try {
-        setUsers();
-        setError();
-        setIsLoading(true);
-
-        const response = await axios.get(`/api/users/${username}`);
-        const userResponse = await response.json();
-
-        setUsers(userResponse);
-      } catch (err) {
-        console.error(err);
-        setError(err);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchUser();
-  }, []);
-
-  return { users, isLoading, error };
-}
-
-const mockInput = "ray_benigno"
 
 const initialState = {
   searchTerm: "",
-  userDetail: {}
+  userDetail: {},
+  getSuggetions: []
 };
 
 const actionTypes = {
   "SEARCH_TERM": "SEARCH_TERM",
   "GET_SUGGESTIONS": "GET_SUGGESTIONS",
-  "ENTER_KEY_PRESSED": "ENTER_KEY_PRESSED"
+  "ENTER_KEY_PRESSED": "ENTER_KEY_PRESSED",
+  "UPDATE_USER_DETAILS": "UPDATE_USER_DETAILS"
 
 }
 
@@ -50,15 +20,15 @@ const reducer = (state, action) => {
   switch (action.type) {
     // Implementation of Inmutability
     case actionTypes.SEARCH_TERM: return Object.assign({}, state, { searchTerm: action.payload })
-    case actionTypes.GET_SUGGESTIONS: return Object.assign({}, state, { userDetail: action.payload })
+    case actionTypes.GET_SUGGESTIONS: return Object.assign({}, state, { getSuggetions: action.payload })
+    case actionTypes.UPDATE_USER_DETAILS: return Object.assign({}, state, { userDetail: action.payload })
     case actionTypes.ENTER_KEY_PRESSED: {
       // dispacher available through action.dispatch
       // chech for user before calling api
       console.log(`serch-term: ${state.searchTerm}`)
       axios.get(`/api/users/${state.searchTerm}`)
         // dispatch update 
-        .then(result => console.log("fetching", result.data))
-      return Object.assign({}, state, { searchTerm: "" })
+        .then(result => action.dispatch({ type: actionTypes.UPDATE_USER_DETAILS, payload: result.data }))
     }
     default:
       return state
@@ -67,16 +37,24 @@ const reducer = (state, action) => {
 
 const handleChange = dispatch => e => dispatch({ type: actionTypes.SEARCH_TERM, payload: e.target.value })
 const handleEnter = dispatch => ({ keyCode, which }) => (keyCode === 13 || which === 13) && dispatch({ type: actionTypes.ENTER_KEY_PRESSED, dispatch })
+const isEmpty = obj => Object.keys(obj).length === 0
+
+const showDetails = props => {
+  const { username, plays, friends, tracks, uri } = props
+  return <div>
+    `${username, plays, friends, uri, tracks}`
+  </div >
+}
 
 export default function MainContent() {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  // action factory that attaches the dispacher
-  const thunk = username => dispatch => { }
+  const [{ searchTerm, userDetail, getSuggetions }, dispatch] = useReducer(reducer, initialState)
+
+  //MVP alpha use useEffect to handle when enter, change in the state
 
   // useEffect(() => {
   //   async function getSuggestions() {
-  //     const { data: userDetails } = await axios.get(`/api/users/ray_benigno`);
-  //     dispatch({ type: actionTypes.GET_SUGGESTIONS, payload: userDetails })
+  //     const { data: userDetails } = await axios.get(`/api/users/suggestions`);
+  //     dispatch({ type: actionTypes.GET_SUGGESTIONS, payload: usersSuggestions })
   //   }
   //   getSuggestions()
   // }, [])
@@ -86,11 +64,15 @@ export default function MainContent() {
       <InnerContainer>
         <Input
           id="input"
-          value={state?.searchTerm}
+          value={searchTerm}
           onChange={handleChange(dispatch)}
           type={"text"}
           onKeyPress={handleEnter(dispatch)}
         />
+        <p>ray_benigno</p>
+        {isEmpty(userDetail) ? console.log("nothing") :
+          showDetails(userDetail)
+        }
       </InnerContainer>
     </>
   );
